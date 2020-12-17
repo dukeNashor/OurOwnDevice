09b625ee40cb573d221a0ccf821b223bfa3e451e
dukeNashor <sonicisme@163.com>
2020-12-17 20:24:39 +0800

    CS233 Problem Set 1

--- helloworld/helloworld/kernel.cu
+++ helloworld/helloworld/kernel.cu
@@ -1,16 +1,27 @@
 
+#include <corecrt_math.h>
+
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <iostream>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+cudaError_t selfPowWrapper(const float* h_in, float* h_out, int size, float num_pow);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
+
+__global__ void selfPowKernel(float* d_out, float* d_in, float num_pow)
+{
+    int idx = threadIdx.x;
+    d_out[idx] = powf(d_in[idx], num_pow);
+}
+
 
 int main()
 {
@@ -37,8 +48,95 @@ int main()
         return 1;
     }
 
+
+    // self pow:
+    float self_pow_arr_in[] = { 1.0f, 1.1f, 1.2f, 1.3f };
+
+    const size_t array_size = sizeof(self_pow_arr_in) / sizeof(float);
+
+    float self_pow_arr_out[array_size] = { 0.0f };
+    const float pow = 2.0f;
+    selfPowWrapper(self_pow_arr_in, self_pow_arr_out, array_size, pow);
+
+    for (auto it = std::begin(self_pow_arr_out), it_end = std::end(self_pow_arr_out); it != it_end; ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
     return 0;
 }
+
+cudaError_t selfPowWrapper(const float* h_in, float* h_out, int size, float num_pow)
+{
+    float *d_in = 0;
+    float *d_out = 0;
+
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&d_in, size * sizeof(float));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&d_out, size * sizeof(float));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy input vectors from host memory to GPU buffers.
+    cudaStatus = cudaMemcpy(d_in, h_in, size * sizeof(float), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    selfPowKernel<<<1, size>>>(d_out, d_in, num_pow);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(h_out, d_out, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(d_in);
+    cudaFree(d_out);
+    
+    return cudaStatus;
+}
+
+
+
+
+
+
 
 // Helper function for using CUDA to add vectors in parallel.
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
